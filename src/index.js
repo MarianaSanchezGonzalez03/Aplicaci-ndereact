190d667d3e19a8db1c6e3e39e1c143c340f846c8
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2022-02-22 19:45:31 -0600

    Nuestro primer componente

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import "./index.css";
 
 const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot = document.querySelector("#app");
 
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render(<PrimeraApp />, divRoot);
 
 
